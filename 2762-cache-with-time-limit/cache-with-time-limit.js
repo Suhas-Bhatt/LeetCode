4a2fe23ecb123e54fc6178fbcd5c141a4389d707
Suhas-Bhatt <121959212+Suhas-Bhatt@users.noreply.github.com>
2025-11-21 17:03:53 +0530

    Time: 52 ms (50.10%) | Memory: 53.8 MB (74.85%) - LeetSync

---
+++ 2762-cache-with-time-limit/cache-with-time-limit.js
@@ -0,0 +1,2 @@
+
+class TimeLimitedCache{constructor(){this.cache=new Map}set(e,t,c){const h=Date.now(),s=this.cache.has(e)&&this.cache.get(e).expiry>h;return this.cache.set(e,{value:t,expiry:h+c}),setTimeout((()=>{this.cache.has(e)&&this.cache.get(e).expiry<=Date.now()&&this.cache.delete(e)}),c),s}get(e){const t=Date.now(),c=this.cache.get(e);return c&&c.expiry>t?c.value:(this.cache.delete(e),-1)}count(){const e=Date.now();let t=0;for(const[c,h]of this.cache)h.expiry>e?t++:this.cache.delete(c);return t}}
