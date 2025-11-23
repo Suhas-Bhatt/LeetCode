3852db1d381d2d8ca52bacbf7bba9cd4ab962ec4
Suhas-Bhatt <121959212+Suhas-Bhatt@users.noreply.github.com>
2025-11-23 17:25:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (5.44%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,10 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            if(target==nums[i]){
-                return i;
-            }
-        }
-        return -1;
+           return searchHelper(nums, target, 0, nums.length - 1);
     }
+
+    private int searchHelper(int arr[],int tar,int si,int ei){
+           //base case
+         if(si>ei){
+            return -1;
+         }
+          //kaam
+         int mid=si+(ei-si)/2;
+    
+          //case FOUND
+          if(arr[mid]==tar){
+                return mid;
+            }
+
+            //mid on L1
+
+            if(arr[si]<=arr[mid]){
+                //case a: left
+                if(arr[si]<=tar && tar<=arr[mid]){
+                    return searchHelper(arr,tar,si,mid-1);
+                } else{
+                //case b:right
+                    return searchHelper(arr,tar,mid+1,ei);
+             }
+    }
+
+    //mid on L2
+    else{
+        //case c:right
+        if(arr[mid]<=tar && tar<=arr[ei]){
+            return searchHelper(arr,tar,mid+1,ei);
+        } else {
+            return searchHelper(arr,tar,si,mid-1);
+        }
+    }
+}
 }
