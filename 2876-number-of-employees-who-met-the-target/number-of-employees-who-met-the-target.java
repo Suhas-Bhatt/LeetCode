06526582bb468a7b574170890d2bd1697cb1fb38
Suhas-Bhatt <121959212+Suhas-Bhatt@users.noreply.github.com>
2026-02-26 21:29:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (96.28%) - LeetSync

--- 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
+++ 2876-number-of-employees-who-met-the-target/number-of-employees-who-met-the-target.java
@@ -2,7 +2,7 @@ class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
         int count=0;
         for(int i=0;i<hours.length;i++){
-            if(target <= hours[i]){
+            if(hours[i] >= target){
                 count++;
             }
         }
