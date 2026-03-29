376ee028798238da52c41fb9ec9e01e80a19f9b6
Suhas-Bhatt <121959212+Suhas-Bhatt@users.noreply.github.com>
2026-03-29 16:41:42 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (19.39%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canBeEqual(String s1, String s2){
+        boolean isEven = (s1.charAt(0)==s2.charAt(0)  && s1.charAt(2)==s2.charAt(2)  )||(s1.charAt(0)==s2.charAt(2)  && s1.charAt(2)==s2.charAt(0));
+
+        boolean isOdd = (s1.charAt(1)==s2.charAt(1)  && s1.charAt(3)==s2.charAt(3)  )||(s1.charAt(1)==s2.charAt(3)  && s1.charAt(3)==s2.charAt(1));
+
+
+        return isEven && isOdd;
+    }
+}
