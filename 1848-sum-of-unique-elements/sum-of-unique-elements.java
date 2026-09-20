60a2330332147c31dbcd06657e99c9115fb81e22
Suhas-Bhatt <121959212+Suhas-Bhatt@users.noreply.github.com>
2026-09-20 22:53:06 +0530

    Time: 5 ms (2.10%) | Memory: 43.5 MB (7.28%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -2,7 +2,7 @@ class Solution {
     public int sumOfUnique(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        if(n == 1) return nums[n-1];
+        if(n == 1) return nums[0];
         if(nums[0] == nums[n-1]) return 0;
         int sum = 0;
         for (int i = 0; i < n; i++) {
